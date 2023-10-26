4a857e8f2a41e58ecd17aa533300922dff077b5f
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-26 09:47:36 +0300

    remove per-package memtag setting overrides
    These packages are opted-in to "async" memtagMode via their AndroidManifests.

--- device-common.mk
+++ device-common.mk
@@ -65,9 +65,6 @@ PRODUCT_VENDOR_PROPERTIES += \
 ifeq (,$(filter %_fullmte,$(TARGET_PRODUCT)))
 ifeq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
 PRODUCT_PRODUCT_PROPERTIES += ro.arm64.memtag.bootctl_supported=1
-PRODUCT_PRODUCT_PROPERTIES += persist.arm64.memtag.app.com.android.se=off
-PRODUCT_PRODUCT_PROPERTIES += persist.arm64.memtag.app.com.google.android.bluetooth=off
-PRODUCT_PRODUCT_PROPERTIES += persist.arm64.memtag.app.com.android.nfc=off
 PRODUCT_PRODUCT_PROPERTIES += persist.arm64.memtag.system_server=off
 endif
 endif
