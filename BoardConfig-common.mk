df864ae5c714cc7af98d368180b949b756f12468
Daniel Micay <daniel.micay@grapheneos.org>
2025-01-11 18:19:06 -0500

    kernel: enable asymmetric MTE in panic mode

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -42,9 +42,9 @@ BOARD_KERNEL_CMDLINE += swiotlb=noforce
 BOARD_KERNEL_CMDLINE += disable_dma32=on
 BOARD_KERNEL_CMDLINE += sysctl.kernel.sched_pelt_multiplier=4
 
-# Normal (non-_fullmte) builds should disable kasan
+# Normal (non-_fullmte) builds
 ifeq (,$(filter %_fullmte,$(TARGET_PRODUCT)))
-BOARD_KERNEL_CMDLINE += kasan=off
+BOARD_KERNEL_CMDLINE += kasan.fault=panic kasan.mode=asymm
 endif
 
 # Enable a limited subset of MTE for "normal" (non-_fullmte) eng builds.
