bd1af8454327bf3a789035e0768131570aaa242a
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-19 12:47:12 +0000

    strip wifi product copy files

--- device.mk
+++ device.mk
@@ -901,10 +901,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
-# WIFI COEX
-PRODUCT_COPY_FILES += \
-	device/google/zumapro/wifi/coex_table.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/coex_table.xml
-
 PRODUCT_PACKAGES += hostapd
 PRODUCT_PACKAGES += wpa_supplicant
 PRODUCT_PACKAGES += wpa_supplicant.conf
