61a09619e88cae4a0c9e8b99fa1e20ab50d60cd6
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-20 12:40:01 +0000

    strip nonexistent gpu soong namespace

--- device.mk
+++ device.mk
@@ -223,15 +223,6 @@ else
 TARGET_USES_VULKAN = true
 endif
 
-# "vendor/arm" doesn't exist in PDK build
-ifeq (,$(realpath $(TOPDIR)vendor/arm/mali/valhall/Android.bp))
-PRODUCT_SOONG_NAMESPACES += \
-	vendor/google_devices/zumapro/prebuilts/gpu
-else
-PRODUCT_SOONG_NAMESPACES += \
-	vendor/arm/mali/valhall
-endif
-
 $(call soong_config_set,pixel_mali,soc,$(TARGET_BOARD_PLATFORM))
 # TODO (b/297408842): The gralloc is being open-sourced, and we cannot pass
 # "zumapro" as SOC to build it. Pass "zuma" as SOC for now.
