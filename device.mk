37963ea4ee9ee200075eda72d0810dee776532c5
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-20 12:28:13 +0000

    strip hardware properties

--- device.mk
+++ device.mk
@@ -246,14 +246,6 @@ PRODUCT_PACKAGES += \
 	libGLESv1_CM_angle \
 	libGLESv2_angle \
 	vulkan.pastel
-
-PRODUCT_VENDOR_PROPERTIES += \
-	ro.hardware.egl=angle \
-	ro.hardware.vulkan=pastel
-else
-PRODUCT_VENDOR_PROPERTIES += \
-	ro.hardware.egl=mali \
-	ro.hardware.vulkan=mali
 endif
 
 # SurfaceFlinger / RenderEngine
