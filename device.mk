33978eede674c2e4bcc30c781ddafb5953bd2df7
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-22 14:44:59 +0000

    strip more audio related upstream TODO comments

--- device.mk
+++ device.mk
@@ -431,24 +431,6 @@ include hardware/google/pixel/rebalance_interrupts/rebalance_interrupts.mk
 # Audio HALs
 #
 
-# Audio Configurations
-#USE_LEGACY_LOCAL_AUDIO_HAL := false
-#USE_XML_AUDIO_POLICY_CONF := 1
-
-# Cannot reference variables defined in BoardConfig.mk, uncomment this if
-# BOARD_USE_OFFLOAD_AUDIO and BOARD_USE_OFFLOAD_EFFECT are true
-## AudioEffectHAL library
-#PRODUCT_PACKAGES += \
-#	libexynospostprocbundle
-
-# Cannot reference variables defined in BoardConfig.mk, uncomment this if
-# BOARD_USE_SOUNDTRIGGER_HAL is true
-#PRODUCT_PACKAGES += \
-#	sound_trigger.primary.maran9820
-
-# A-Box Service Daemon
-#PRODUCT_PACKAGES += main_abox
-
 # Libs
 PRODUCT_PACKAGES += \
 	com.android.future.usb.accessory
