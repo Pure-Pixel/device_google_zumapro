6719cfc3a29c6640ae78ec1ecdf719fff8b59a37
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 10:05:25 +0000

    strip incremental props

--- device.mk
+++ device.mk
@@ -779,10 +779,6 @@ endif
 # Enable to build standalone vendor_kernel_boot image.
 PRODUCT_BUILD_VENDOR_KERNEL_BOOT_IMAGE := true
 
-# Enable Incremental on the device
-PRODUCT_PROPERTY_OVERRIDES += \
-	ro.incremental.enable=true
-
 # Project
 include hardware/google/pixel/common/pixel-common-device.mk
 
