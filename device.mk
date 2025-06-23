e8b7220004730f023a8a9242e4d91971b9a60af4
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 10:07:56 +0000

    strip filename encryption for adoptable storage props

--- device.mk
+++ device.mk
@@ -796,10 +796,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml
 
-# Use HCTR2 for filenames encryption on adoptable storage.
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.crypto.volume.options=aes-256-xts:aes-256-hctr2
-
 # Hardware Info Collection
 include hardware/google/pixel/HardwareInfo/HardwareInfo.mk
 
