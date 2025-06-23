c93a3d0562d23b1a7b542d59076380dc831ed16f
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 09:43:39 +0000

    strip more display related props

--- device.mk
+++ device.mk
@@ -525,10 +525,6 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
 	vendor.camera.multicam.enable_p23_multicam=true
 
-# Disable dimming by default
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.display.0.brightness.dimming.usage?=2
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.display.1.brightness.dimming.usage?=2
-
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.sys.sf.native_mode=2
 
