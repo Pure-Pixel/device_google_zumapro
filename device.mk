5bb67f5604adaf67df66e50284719f5cc050d6bb
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 10:03:45 +0000

    strip thermal hal genl props

--- device.mk
+++ device.mk
@@ -749,9 +749,6 @@ PRODUCT_PACKAGES += \
 ## Start packet router
 include device/google/gs101/telephony/pktrouter.mk
 
-# Thermal HAL
-PRODUCT_PROPERTY_OVERRIDES += persist.vendor.enable.thermal.genl=true
-
 # EdgeTPU
 include device/google/gs-common/edgetpu/edgetpu.mk
 # Config variables for TPU chip on device.
